06f9cc3f9dfeb9c55b0ac60faa7d0ac1ed70855a
hwblue24 <125501572+hwblue24@users.noreply.github.com>
2026-06-10 16:45:30 -0400

    set up inputs in children for flexibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-function InputCards({info}) {
+function InputCards({info, children}) {
   return (
     <>
     <h1>{info.title}</h1>
     <div>
-      
-      <input type="text" />
+      {children}
     </div>
-    
     <button>Edit</button>
     <button>Submit</button>
     </>
@@ -17,17 +15,33 @@ function InputCards({info}) {
 function App () {
   return (
     <>
-      <InputCards info={{
-        title:'Personal Information'
-      }} />
-       <InputCards info={{
-        title:'Educational Experience'
-      }} />
-       <InputCards info={{
-        title:'Practical Experience'
-      }} />
-    </>
-  )
+    <InputCards info={{ title:'Personal Information'}} >
+      <div>
+        <input type="text" placeholder="name" />
+        <input type="text" placeholder="age" />
+        <input type="text" placeholder="birth"/>
+      </div>
+    </InputCards>
+
+    <InputCards info={{ title:'Educational Information'}} >
+    <div>
+      <input type="text" placeholder="School name" />
+      <input type="text" placeholder="Degree" />
+      <input type="text" placeholder="Graduation date"/>
+    </div>
+  </InputCards>
+
+  <InputCards info={{ title:'Practical Experience'}} >
+    <div>
+      <input type="text" placeholder="Company name" />
+      <input type="text" placeholder="Position Title" />
+      <input type="text" placeholder="Responsibilities"/>
+      <input type="text" placeholder="Date Start"/>
+      <input type="text" placeholder="Date End"/>
+    </div>
+  </InputCards>
+  </>
+  );
 }
 
 export default App
