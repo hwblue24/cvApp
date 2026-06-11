e14c37b5979ddc778026e153a190664768384102
hwblue24 <125501572+hwblue24@users.noreply.github.com>
2026-06-10 23:13:49 -0400

    submit button changes input to html using state

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,49 @@
-function InputCards({info, children}) {
+import {useState} from 'react';
+
+function InputCards({info, inputs, list, submit, handleSubmitClick}) {
+  
   return (
     <>
     <h1>{info.title}</h1>
-    <div>
-      {children}
-    </div>
+    {!submit? inputs : list}
     <button>Edit</button>
-    <button>Submit</button>
+    <button onClick ={handleSubmitClick}>
+      Submit</button>
     </>
   )
 }
 
 
 function App () {
+  const [submit, setSubmit] = useState(false);
+
+  const inputs = (
+  <div>
+    <input type="text" placeholder="name" />
+    <input type="text" placeholder="age" />
+    <input type="text" placeholder="birth"/>
+  </div>
+  )
+
+  const list = (
+  <ol>
+    <li>name</li>
+    <li>age</li>
+    <li>birth</li>
+  </ol>
+  )
+
   return (
     <>
-    <InputCards info={{ title:'Personal Information'}} >
-      <div>
-        <input type="text" placeholder="name" />
-        <input type="text" placeholder="age" />
-        <input type="text" placeholder="birth"/>
-      </div>
-    </InputCards>
+    <InputCards info={{ title:'Personal Information'}} 
+      handleSubmitClick ={()=>setSubmit(!submit)}
+      inputs = {inputs}
+      list = {list}
+      submit={submit}
+      setSubmit={setSubmit}
+    />
 
-    <InputCards info={{ title:'Educational Information'}} >
+    {/*<InputCards info={{ title:'Educational Information'}} >
     <div>
       <input type="text" placeholder="School name" />
       <input type="text" placeholder="Degree" />
@@ -39,7 +59,7 @@ function App () {
       <input type="text" placeholder="Date Start"/>
       <input type="text" placeholder="Date End"/>
     </div>
-  </InputCards>
+  </InputCards>*/}
   </>
   );
 }
