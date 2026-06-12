83524bedccbc5a715a76ff5cc6f8d3c7a5068577
hwblue24 <125501572+hwblue24@users.noreply.github.com>
2026-06-12 11:15:28 -0400

    moved submit and edit logic into child component so each compontent instance works independently of each other

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import {useState} from 'react';
 
-function InputCards({info, inputs, list, submit, handleSubmitClick}) {
-  
+function InputCards({outPut}) {
+  const [submit, setSubmit] = useState(0);
+  const isSubmit = (submit === 0)
+
   return (
     <>
-    <h1>{info.title}</h1>
-    {!submit? inputs : list}
-    <button>Edit</button>
-    <button onClick ={handleSubmitClick}>
+    <h1>{outPut.title}</h1>
+
+    {isSubmit ? outPut.inputs : outPut.list}
+
+    <button onClick = {()=>setSubmit(0)}>
+      Edit
+    </button>
+    <button onClick ={()=>setSubmit(1)}>
       Submit</button>
     </>
   )
@@ -15,51 +21,51 @@ function InputCards({info, inputs, list, submit, handleSubmitClick}) {
 
 
 function App () {
-  const [submit, setSubmit] = useState(false);
-
-  const inputs = (
-  <div>
-    <input type="text" placeholder="name" />
-    <input type="text" placeholder="age" />
-    <input type="text" placeholder="birth"/>
-  </div>
-  )
-
-  const list = (
-  <ol>
-    <li>name</li>
-    <li>age</li>
-    <li>birth</li>
-  </ol>
-  )
-
   return (
     <>
-    <InputCards info={{ title:'Personal Information'}} 
-      handleSubmitClick ={()=>setSubmit(!submit)}
-      inputs = {inputs}
-      list = {list}
-      submit={submit}
-      setSubmit={setSubmit}
+    <InputCards 
+      outPut = {{
+        title:'Personal Information',
+        inputs: 
+        <div>
+          <input type="text" placeholder="name" />
+          <input type="text" placeholder="age" />
+          <input type="text" placeholder="birth"/>
+        </div>,
+        list: 
+        <ol>
+          <li>name</li>
+          <li>age</li>
+          <li>birth</li>
+        </ol>
+      
+
+      }}
+      
     />
 
-    {/*<InputCards info={{ title:'Educational Information'}} >
-    <div>
-      <input type="text" placeholder="School name" />
-      <input type="text" placeholder="Degree" />
-      <input type="text" placeholder="Graduation date"/>
-    </div>
-  </InputCards>
+<InputCards 
+      outPut = {{
+        title:'Work Information',
+        inputs: 
+        <div>
+          <input type="text" placeholder="Job" />
+          <input type="text" placeholder="start" />
+          <input type="text" placeholder="end"/>
+        </div>,
+        list: 
+        <ol>
+          <li>job</li>
+          <li>start</li>
+          <li>end</li>
+        </ol>
+      
 
-  <InputCards info={{ title:'Practical Experience'}} >
-    <div>
-      <input type="text" placeholder="Company name" />
-      <input type="text" placeholder="Position Title" />
-      <input type="text" placeholder="Responsibilities"/>
-      <input type="text" placeholder="Date Start"/>
-      <input type="text" placeholder="Date End"/>
-    </div>
-  </InputCards>*/}
+      }}
+   
+    />
+
+
   </>
   );
 }
